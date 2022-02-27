0c95de92a642ddbc48d4c30331d6a9b327c51656
Woosang <yws1502@naver.com>
2022-02-27 17:04:45 +0900

    💄 expense form 컴포넌트 스타일링

--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -1,31 +1,34 @@
+import "./ExpenseForm.css";
 import Card from "../UI/Card";
 
 function ExpenseForm(props) {
   return (
-    <Card>
+    <Card className="expense-form-cont">
       <h2 className="a11y-hidden">지출 생성 영역</h2>
       <form>
-        <fieldset>
-          <label>Title</label>
-          <input type="text" placeholder="지출 내역을 입력하세요" />
-        </fieldset>
-        <fieldset>
-          <label>Price</label>
-          <input
-            type="number"
-            min="0"
-            step="100"
-            placeholder="숫자만 입력 가능합니다."
-          />
-        </fieldset>
-        <fieldset>
-          <label>Date</label>
-          <input type="date" />
-        </fieldset>
+        <div className="input-cont">
+          <fieldset>
+            <label>Title</label>
+            <input type="text" placeholder="지출 내역을 입력하세요" />
+          </fieldset>
+          <fieldset>
+            <label>Price</label>
+            <input
+              type="number"
+              min="0"
+              step="100"
+              placeholder="숫자만 입력 가능합니다."
+            />
+          </fieldset>
+          <fieldset>
+            <label>Date</label>
+            <input type="date" />
+          </fieldset>
+        </div>
+        <div className="date-cont">
+          <button type="submit">지출 💰</button>
+        </div>
       </form>
-      <div>
-        <button type="submit">지출💰</button>
-      </div>
     </Card>
   );
 }
